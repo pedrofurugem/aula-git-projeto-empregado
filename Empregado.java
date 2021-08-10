3cb7d77fbb02d1968efede386b6358a29338e5ff
Pedro <pedrofuruguem@hotmail.com>
2021-08-10 19:51:27 -0300

    Processamento de empregados usando a lista

--- Empregado.java
+++ Empregado.java
@@ -16,20 +16,24 @@ public class Empregado{
     }
 
     //adicionar à classe Empregado.java
-public void setSalario (double salario){
+  public void setSalario (double salario){
     this.salario = salario;
    }
    public double getSalario (){
     return this.salario;
+
    }
    public void setComissao (double comissao){
     this.comissao = comissao;
    }
+
    public double getComissao (){
     return this.comissao;
    }
+
    public void setBonus (double bonus){
     this.bonus = bonus;
+    
    }
    public double getBonus (){
     return this.bonus;
